1735d1baa05e3b69446c2c0eba5e920f27bee9f9
BiggerThings <nnsbiggerthings@gmail.com>
2026-06-30 23:42:16 +0200

    Testing: Created third and fourth test for Card Component using RTL with render method to get alt text and src of image

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,8 +3,10 @@ import { render } from '@testing-library/react';
 
 describe('Card component', () => {
     it('renders correctly with given props', () => {
-        const { getByText } = render(<Card id={1} name="John Doe" email="j@mail.com" />);
+        const { getByText, getByAltText } = render(<Card id={1} name="John Doe" email="j@mail.com" />);
         expect(getByText('John Doe')).toBeInTheDocument();
         expect(getByText('j@mail.com')).toBeInTheDocument();
+        expect(getByAltText('robots')).toBeInTheDocument();
+        expect(getByAltText('robots').src).toBe('https://robohash.org/1?200x200');
     });
 })
