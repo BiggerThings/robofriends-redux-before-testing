7e0ed0722065f65d3580592af96786d5c3167a91
BiggerThings <nnsbiggerthings@gmail.com>
2026-06-30 23:19:49 +0200

    Testing: Created first test suite with first test for Card Component

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
 import Card from './Card';
 
-console.log(shallow(<Card />));
+describe('Card component', () => {
+    it('renders correctly with given props', () => {
+        // const props = {
+        //     name: 'John Doe',
+        //     email: 'j@mail.com',
+        //     id: 1
+        // };
+        // const wrapper = shallow(<Card {...props} />);
+        // expect(wrapper).toMatchSnapshot();
+    });
+})
