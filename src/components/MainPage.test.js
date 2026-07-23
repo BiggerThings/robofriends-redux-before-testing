7b8d5392a8d4b8cc4addddde724eee70303ef0de
BiggerThings <nnsbiggerthings@gmail.com>
2026-07-23 23:23:57 +0200

    Tested state and class methods for MainPage Class Component

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -1,12 +1,42 @@
 import React from "react";
-import App from "./App";
 import { render } from "@testing-library/react";
+import MainPage from "./MainPage";
+import { createRef } from 'react';
 
-it("expect renders App component", () => {
+let wrapper;
+let wrapper2;
+let ref;
+let ref2;
+
+beforeEach(() => {
     const mockStore = {
+        onRequestRobots: jest.fn(),
         robots: [],
         searchField: "",
         isPending: false,
     }
-    render(<App store={mockStore} />);
+
+    var mockStore2 = {
+        onRequestRobots: jest.fn(),
+        robots: [{
+            id: 1,
+            name: "Jack",
+            email: "jack@example.com"
+        }],
+        searchField: "a",
+        isPending: false,
+    }
+    ref = createRef();
+    ref2 = createRef();
+    wrapper = render(<MainPage {...mockStore} ref={ref} />);
+    wrapper2 = render(<MainPage {...mockStore2} ref={ref2} />);
 });
+
+it("expect renders MainPage component", () => {
+    expect(wrapper).toBeTruthy();
+});
+
+it("filters robots correctly", () => {
+    expect(ref.current.filterRobots()).toEqual([]);
+    expect(ref2.current.filterRobots()).toHaveLength(1);
+})
