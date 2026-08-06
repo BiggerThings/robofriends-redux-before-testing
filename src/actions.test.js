bf923cdf52c487d560c628c9ac47349c5d16e5ae
BiggerThings <nnsbiggerthings@gmail.com>
2026-08-06 22:25:37 +0200

    Testing: Action (TBC)

---
+++ src/actions.test.js
@@ -0,0 +1,36 @@
+import * as actions from './actions';
+import {
+    CHANGE_SEARCHFIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants'
+
+import apiCall from './api/api';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const mockStore = configureMockStore([thunk]);
+
+describe('actions', () => {
+    it('should create an action to search robots', () => {
+        const text = 'woo';
+
+        const expectedAction = {
+            type: CHANGE_SEARCHFIELD,
+            payload: text
+        }
+        expect(actions.setSearchField(text)).toEqual(expectedAction);
+    })
+
+    it('should create actions to request robots (API)', () => {
+        const store = mockStore;
+        store.dispatch(actions.requestRobots());
+        const action = store.getActions();
+        const expectedAction = {
+            type: REQUEST_ROBOTS_PENDING
+        }
+        expect(action[0]).toEqual(expectedAction);
+
+    })
+})
